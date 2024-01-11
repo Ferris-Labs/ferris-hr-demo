c663e754f739fe26bd1294f03a90a15e2f69e035
tomthebuzz <tom.debus@integrationalpha.com>
2024-01-11 16:36:09 +0100

    Debug

--- 2a_extract_skills_job/app.py
+++ 2a_extract_skills_job/app.py
@@ -9,10 +9,9 @@ job_file = context.params.get("job_file") or ""
 job_text = context.params.get("job_text") or ""
 
 # Setup LangChain with OpenAI API
-# openai_api_key = os.getenv("OPENAI_API_KEY")
-OpenAI.api_key = context.config.get('OPENAI_API_KEY')
+oai_key = context.config.get('OPENAI_API_KEY')
 # openai_api_key = "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"
-client = OpenAI()
+client = OpenAI(api_key=oai_key)
 
 
 
